5638e8b49012d4941fd17d7ee345e77aa3d568ff
jolimba <joovictors85@gmail.com>
2022-08-25 01:22:14 -0300

    banco config

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,11 +4,11 @@ import { Users } from "./entity/Users"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "ec2-34-227-135-211.compute-1.amazonaws.com",
+    host: "ec2-34-247-72-29.eu-west-1.compute.amazonaws.com",
     port: 5432,
-    username: "ywwrtcftujlcdw",
-    password: "6e6c1c8bcc37258160df90c3fc4a7c0550ee519d77ff74839ea07938a2c7f44f",
-    database: "d7jvbbg96qrart",
+    username: "tmzmfdipthwvqa",
+    password: "0f0b74a647b689c96dc79b2ce3ceb62e12d4b1d49d839e6c846abd766e125659",
+    database: "dcjiqp6so4iqth",
     synchronize: true,
     logging: false,
     entities: [Users],
